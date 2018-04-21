277df936a224bc559cba7370411f8ae61d846a78
stvyang <stv.yang10@gmail.com>
2018-04-22 00:40:15 +0700

    Update and Delete Book feature

--- server/mutations/book.js
+++ server/mutations/book.js
@@ -1,7 +1,8 @@
 const graphql = require('graphql');
 
 const {
-
+  GraphQLID,
+  GraphQLBoolean
 } = graphql;
 
 const Book = require('../models/book');
@@ -16,6 +17,20 @@ const mutation = {
       let book = new Book(args.data);
       return book.save();
     }
+  },
+  updateBook: {
+    type: GraphQLBoolean,
+    args: { id: { type: GraphQLID }, data: { type: BookInputType } },
+    resolve(parent, args) {
+      return Book.updateOne({ _id: args.id }, args.data);
+    }
+  },
+  deleteBook: {
+    type: GraphQLBoolean,
+    args: { id: { type: GraphQLID } },
+    resolve(parent, args) {
+      return Book.deleteOne({ _id: args.id });
+    },
   }
 }
 
