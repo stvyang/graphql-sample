44fe48c34b5a1258066ab69c19625b142c153c90
stvyang <stv.yang10@gmail.com>
2018-04-22 16:07:30 +0700

    Change server port to environment variable

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 const schema = require('./schema');
 const app = express();
+const port = process.env.PORT || 4000;
 
 mongoose.connect('mongodb://stvyang:password@ds231245.mlab.com:31245/graphql-sample');
 mongoose.connection.once('open', () => {
@@ -15,6 +16,6 @@ app.use('/graphql', graphqlHTTP({
 	graphiql: true
 }))
 
-app.listen(4000, () => {
-	console.log('apps is listening on port 4000');
+app.listen(port, () => {
+	console.log(`apps is listening on port ${port}`);
 })
